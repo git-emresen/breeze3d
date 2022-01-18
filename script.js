4c172596a59e28fd7aa3ede575cb7c5a1e69080a
siyahand <siyah.and@gmail.com>
2022-01-18 20:38:05 +0300

    v3_1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+/* function loadDoc(){
+const req=new XMLHttpRequest();
+req.onload=function(){ 
+    document.getElementById("content").innerHTML=this.responseText;
+}
+req.open("GET","http://localhost:3000/profile");
+req.send();
+} */
+
+
